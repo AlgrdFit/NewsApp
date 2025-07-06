5f3bfa775e91494e9c34d07ebc97dac1cabf091f
AlgrdFit <Algirdas.Fitkevicius@gmail.com>
2025-07-06 20:51:42 +0300

    implemented onMessageReceived to check if the message was received

--- app/src/main/java/com/telesoftas/newsapp/services/CloudMessagingService.kt
+++ app/src/main/java/com/telesoftas/newsapp/services/CloudMessagingService.kt
@@ -2,11 +2,15 @@ package com.telesoftas.newsapp.services
 
 import android.util.Log
 import com.google.firebase.messaging.FirebaseMessagingService
+import com.google.firebase.messaging.RemoteMessage
 
 class CloudMessagingService : FirebaseMessagingService() {
 
     override fun onNewToken(token: String) {
         super.onNewToken(token)
         Log.d("CloudMessagingService", "firebase token registered: $token")
+    }
+    override fun onMessageReceived(remoteMessage: RemoteMessage) {
+        Log.d("CloudMessagingService", "Message received: $remoteMessage.")
     }
 }
